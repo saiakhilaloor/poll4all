7641e6fea5d25f5a1c54a095011565c70e8f60cf
saiakhilaloor <saiakhil.aloor7@gmail.com>
2017-05-20 17:08:19 +0530

    Initial changes

--- polls/models.py
+++ polls/models.py
@@ -3,4 +3,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django.utils.encoding import python_2_unicode_compatible
+
 # Create your models here.
+@python_2_unicode_compatible
+class Question(models.Model):
+    ques_text = models.CharField(max_length=200)
+    ques_date = models.DateTimeField('Date Published')
+    def __str__(self):
+        return self.ques_text
+
+@python_2_unicode_compatible
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
