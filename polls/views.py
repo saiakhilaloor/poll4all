11dea25eaeab8f5ee886c3c33370cf8829b074f1
saiakhilaloor <saiakhil.aloor7@gmail.com>
2017-05-24 10:10:28 +0530

    Some progress

--- polls/views.py
+++ polls/views.py
@@ -3,17 +3,26 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.template import loader
+from django.shortcuts import render
+from django.http import Http404
+
 # Create your views here.
 from django.http import HttpResponse
 from .models import Question
 
 def index(request):
     latest_ques = Question.objects.order_by('-ques_date')[:5]
-    output = ','.join(q.ques_text for q in latest_ques)
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = {"latest_ques":latest_ques,}
+    return HttpResponse(template.render(context,request))
 
 def details(request, question_id):
-    return HttpResponse("This is Question number : %s" %question_id)
+    try:
+        ques = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exists")
+    return render(request, 'polls/details.html',{"ques":ques,})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
